db0b6a53bf039ace92c6eaae166919632f77bc06
Gmorgan87 <gmorgan87@hotmail.co.uk>
2020-02-07 17:19:53 +0000

    adds tables to database in bike_shop.sql

---
+++ db/bike_shop.sql
@@ -0,0 +1,31 @@
+DROP TABLE stock;
+DROP TABLE products;
+DROP TABLE manufacturers;
+DROP TABLE categories;
+
+CREATE TABLE manufacturers (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(255),
+  contact_details TEXT
+);
+
+CREATE TABLE categories (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(255)
+);
+
+CREATE TABLE products (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(255),
+  description TEXT,
+  buy_cost INT,
+  sell_price INT,
+  manufacturer_id INT REFERENCES manufacturers(id),
+  category_id INT REFERENCES categories(id)
+);
+
+CREATE TABLE stock (
+  id SERIAL PRIMARY KEY,
+  product_id INT REFERENCES products(id),
+  quantity INT
+);
